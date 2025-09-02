fb3de34354865b1ddd94617ee2c81df6a2648918
kazukifukuyama <p.t.c_vdl_blue@icloud.com>
2025-09-02 22:27:54 +0900

    fix: CloudFront Origin Access Identityに戻し、ディレクトリアクセス問題を解決

--- terraform/modules/cloudfront/main.tf
+++ terraform/modules/cloudfront/main.tf
@@ -11,16 +11,13 @@ resource "aws_cloudfront_distribution" "main" {
   wait_for_deployment = false
   default_root_object = "index.html"
 
-  # オリジン設定（S3ウェブサイトエンドポイント使用）
+  # オリジン設定（S3 Origin Access Identity使用）
   origin {
-    domain_name = "${var.s3_bucket_id}.s3-website-ap-northeast-1.amazonaws.com"
+    domain_name = "${var.s3_bucket_id}.s3.amazonaws.com"
     origin_id   = "S3-Website-${var.s3_bucket_id}"
 
-    custom_origin_config {
-      http_port              = 80
-      https_port             = 443
-      origin_protocol_policy = "http-only"
-      origin_ssl_protocols   = ["TLSv1.2"]
+    s3_origin_config {
+      origin_access_identity = var.origin_access_identity_path
     }
   }
 
@@ -32,12 +29,7 @@ resource "aws_cloudfront_distribution" "main" {
     viewer_protocol_policy = var.enable_https ? "redirect-to-https" : "allow-all"
     compress               = var.enable_compression
 
-    forwarded_values {
-      query_string = false
-      cookies {
-        forward = "none"
-      }
-    }
+    cache_policy_id = aws_cloudfront_cache_policy.main.id
 
     min_ttl     = var.min_ttl
     default_ttl = var.default_ttl
@@ -107,5 +99,7 @@ resource "aws_cloudfront_cache_policy" "main" {
     query_strings_config {
       query_string_behavior = "none"
     }
+    enable_accept_encoding_brotli = false
+    enable_accept_encoding_gzip   = true
   }
 }
